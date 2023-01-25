4d399ff0601989e0a22b9a33999c107dc6f18eaa
Kirk Lin <linkirk@163.com>
2023-01-25 14:54:50 +0800

    fix(build): build assets error

--- src/components/Application/src/AppLogo.vue
+++ src/components/Application/src/AppLogo.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { computed } from "vue";
 import { PageConstant } from "~/constant/pageConstant";
+import logo from "~/assets/logo.png";
 const props = defineProps({
   /**
    * Whether to show title
@@ -20,7 +21,7 @@ function goHome() {
 
 <template>
   <div :class="`text-${titleSize}`" class="flex items-center justify-center font-semibold dark:text-white" @click="goHome">
-    <img src="src/assets/logo.png" class="m-4 h-11" alt="Logo">
+    <img :src="logo" class="m-4 h-11" alt="Logo">
     <span v-show="showTitle">{{ AppName }}</span>
   </div>
 </template>
