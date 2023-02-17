81c78e47ca8c534a67573846a16034a1267ef17b
Kirk Lin <linkirk@163.com>
2023-02-17 20:32:00 +0800

    fix(utils): deepMerge

--- packages/web/utils/src/util.ts
+++ packages/web/utils/src/util.ts
@@ -1,7 +1,8 @@
 import { isObject } from "@vueuse/core";
+import { cloneDeep } from "lodash-es";
 export function deepMerge<T = any>(src: any = {}, target: any = {}): T {
   let key: string;
-  const res: any = { ...src };
+  const res: any = cloneDeep(src);
   for (key in target) {
     res[key] = isObject(res[key]) ? deepMerge(res[key], target[key]) : (res[key] = target[key]);
   }
