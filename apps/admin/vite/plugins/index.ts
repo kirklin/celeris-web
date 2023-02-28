4fe3572228e866f112c3967cf306eb99b289d085
Kirk Lin <linkirk@163.com>
2023-02-28 17:41:38 +0800

    docs: add comments

--- apps/admin/vite/plugins/index.ts
+++ apps/admin/vite/plugins/index.ts
@@ -5,25 +5,42 @@ import type { ViteEnv } from "../utils";
 import { configUnoCSSPlugin } from "./unocss";
 import { configUNPluginAutoImportPlugin } from "./unpluginAutoImport";
 import { configUNPluginVueComponents } from "./unpluginVueComponets";
+
+/**
+ * Configure the Vite plugins.
+ *
+ * @param root The root directory of the project.
+ * @param viteEnv The Vite environment variables.
+ * @param isBuild Whether the current command is for a production build.
+ */
 export function configVitePlugins(
   root: string,
   viteEnv: ViteEnv,
   isBuild: boolean,
-) {
-  const vitePlugins: (PluginOption | PluginOption[])[] = [
-    vue(),
-  ];
-
+): Array<PluginOption | PluginOption[]> {
+  const vitePlugins: Array<PluginOption | PluginOption[]> = [];
+  // Add the Vue plugin.
+  // 添加 Vue 插件
+  vitePlugins.push(vue());
+  // Add the unplugin-auto-import plugin.
+  // 添加 unplugin-auto-import 插件
   // https://github.com/antfu/unplugin-auto-import
   vitePlugins.push(configUNPluginAutoImportPlugin());
 
+  // Add the unplugin-vue-components plugin.
+  // 添加 unplugin-vue-components 插件
   // https://github.com/antfu/unplugin-vue-components
   vitePlugins.push(configUNPluginVueComponents());
 
+  // Add the UnoCSS plugin.
+  // 添加 UnoCSS 插件
   vitePlugins.push(configUnoCSSPlugin());
+
   // The following plugins only work in the production environment
+  // 生产环境才会添加的插件
   if (isBuild) {
-  //
+    //
   }
+
   return vitePlugins;
 }
