a2fe2d6974587367d88fd2c065686fc79895caf0
Kirk Lin <linkirk@163.com>
2023-03-20 21:06:54 +0800

    feat(router): add router http guard

--- apps/admin/src/router/guard/index.ts
+++ apps/admin/src/router/guard/index.ts
@@ -1,9 +1,12 @@
 import type { RouteLocationNormalized, Router } from "vue-router";
+import { AxiosCanceler } from "@celeris/request";
+import ProjectConfig from "~/config/projectConfig";
 import { notifyRouteChange } from "~/router/mitt/routeChange";
 
 // Don't change the order of creation
 export function setupRouterGuard(router: Router) {
   createPageGuard(router);
+  createHttpGuard(router);
 }
 
 function createPageGuard(router: Router): void {
@@ -25,5 +28,35 @@ function createPageGuard(router: Router): void {
 
   router.afterEach((to: RouteLocationNormalized) => {
     markPageAsLoaded(to);
+  });
+}
+
+/**
+ * 创建一个HTTP守卫，当路由切换时取消所有未完成的请求。
+ * @param router - 路由对象。
+ */
+function createHttpGuard(router: Router) {
+  // 定义一个闭包，用来缓存AxiosCanceler的实例
+  let axiosCanceler: AxiosCanceler | null = null;
+  // 定义一个函数，根据项目配置返回是否需要移除所有未完成的HTTP请求的标志，并创建或销毁AxiosCanceler实例
+  function shouldCancelAllRequests() {
+    const shouldCancelAllRequests = ProjectConfig.shouldRemoveAllHttpPending;
+    if (shouldCancelAllRequests && !axiosCanceler) {
+      // 如果需要移除，并且没有AxiosCanceler实例，则创建一个
+      axiosCanceler = new AxiosCanceler();
+    } else if (!shouldCancelAllRequests && axiosCanceler) {
+      // 如果不需要移除，并且有AxiosCanceler实例，则销毁它
+      axiosCanceler = null;
+    }
+    return shouldCancelAllRequests;
+  }
+
+  router.beforeEach(() => {
+    // 如果需要移除，则调用AxiosCanceler实例的方法取消所有未完成的请求
+    if (shouldCancelAllRequests() && axiosCanceler) {
+      axiosCanceler.removeAllPending();
+    }
+    // 返回true表示允许路由切换
+    return true;
   });
 }
