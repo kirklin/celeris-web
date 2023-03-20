d5adc9014f551ce165dbb915c6d37696158fb093
Kirk Lin <linkirk@163.com>
2023-03-20 21:31:46 +0800

    style(router): add code comments

--- apps/admin/src/router/guard/index.ts
+++ apps/admin/src/router/guard/index.ts
@@ -6,17 +6,29 @@ import ProjectConfig from "~/config/projectConfig";
 import { notifyRouteChange } from "~/router/mitt/routeChange";
 
 // Don't change the order of creation
+// 不要改变创建的顺序
 export function setupRouterGuard(router: Router) {
   createPageGuard(router);
   createHttpGuard(router);
   createProgressGuard(router);
 }
 
+/**
+ * 创建一个页面守卫，用来标记已经加载过的页面并通知路由变化
+ * creates a page guard that marks the loaded pages and notifies the route change
+ * @param router - 路由对象。
+ */
 export function createPageGuard(router: Router): void {
+  // Define a set to store the loaded pages
+  // 定义一个集合，用来存储已经加载过的页面
   const loadedPages = new Set<string>();
+  // Define a function that determines whether a page has been loaded
+  // 定义一个函数，用来判断一个页面是否已经加载过
   function isPageAlreadyLoaded(to: RouteLocationNormalized): boolean {
     return loadedPages.has(to.path);
   }
+  // Define a function that marks a page as loaded
+  // 定义一个函数，用来把一个页面标记为已经加载过
   function markPageAsLoaded(to: RouteLocationNormalized): void {
     if (!isPageAlreadyLoaded(to)) {
       loadedPages.add(to.path);
@@ -36,12 +48,15 @@ export function createPageGuard(router: Router): void {
 
 /**
  * 创建一个HTTP守卫，当路由切换时取消所有未完成的请求。
+ * Creates an HTTP guard that cancels all pending requests when the route is switched.
  * @param router - 路由对象。
  */
 export function createHttpGuard(router: Router) {
   // 定义一个闭包，用来缓存AxiosCanceler的实例
+  // Define a closure to cache the AxiosCanceler instance
   let axiosCanceler: AxiosCanceler | null = null;
   // 定义一个函数，根据项目配置返回是否需要移除所有未完成的HTTP请求的标志，并创建或销毁AxiosCanceler实例
+  // Define a function that returns whether all pending HTTP requests need to be removed according to the project configuration, and creates or destroys the AxiosCanceler instance
   function shouldCancelAllRequests() {
     const shouldCancelAllRequests = ProjectConfig.shouldRemoveAllHttpPending;
     if (shouldCancelAllRequests && !axiosCanceler) {
@@ -56,6 +71,7 @@ export function createHttpGuard(router: Router) {
 
   router.beforeEach(() => {
     // 如果需要移除，则调用AxiosCanceler实例的方法取消所有未完成的请求
+    // If you need to remove, call the AxiosCanceler instance’s method to cancel all pending requests
     if (shouldCancelAllRequests() && axiosCanceler) {
       axiosCanceler.removeAllPending();
     }
@@ -66,12 +82,14 @@ export function createHttpGuard(router: Router) {
 
 /**
  * 创建一个进度条守卫，当路由切换时显示或隐藏进度条
+ * Creates a progress bar guard that shows or hides the progress bar when the route is switched
  * @param router - 路由对象。
  */
 export function createProgressGuard(router: Router) {
   const { getShouldOpenNProgress } = useTransitionSetting();
   router.beforeEach((to) => {
     // 如果目标路由已经加载过，则直接放行
+    // If the target route has been loaded, pass directly
     if (to.meta.loaded) {
       return true;
     }
