6889becb5a77720109489068a916e2b03fd7acbc
Kirk Lin <linkirk@163.com>
2023-04-06 23:48:14 +0800

    feat(type): add AnyFn PromisifyFn

--- packages/web/types/src/global.d.ts
+++ packages/web/types/src/global.d.ts
@@ -4,15 +4,22 @@ export declare global {
   // define global
   const __VITE_USE_MOCK__: boolean;
 
-   type DeepPartial<T> = {
-     [P in keyof T]?: DeepPartial<T[P]>;
-   };
-   type Recordable<T = any> = Record<string, T>;
-   type Nullable<T> = T | null;
-   interface ReadonlyRecordable<T = any> {
-     readonly [key: string]: T;
-   }
+  type AnyFn = (...args: any[]) => any;
+  type Awaitable<T> = Promise<T> | T;
+  type ArgumentsType<T> = T extends (...args: infer U) => any ? U : never;
+  type PromisifyFn<T extends AnyFn> = (
+    ...args: ArgumentsType<T>
+  ) => Promise<ReturnType<T>>;
+
+  type DeepPartial<T> = {
+    [P in keyof T]?: DeepPartial<T[P]>;
+  };
+  type Recordable<T = any> = Record<string, T>;
+  type Nullable<T> = T | null;
+  interface ReadonlyRecordable<T = any> {
+    readonly [key: string]: T;
+  }
+
   // vue
   type PropType<T> = VuePropType<T>;
-
 }
