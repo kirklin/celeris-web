9257085c894a77c4bd6a9da6c37f482f640de427
Kirk Lin <linkirk@163.com>
2023-04-09 23:51:29 +0800

    feat(store): add permission store

---
+++ apps/admin/src/store/modules/permission.ts
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+
+const APP_PERMISSION_ID = "APP_PERMISSION";
+
+export const usePermissionStore = defineStore({
+  id: APP_PERMISSION_ID,
+  state: () => ({
+  }),
+});
+
+// Need to be used outside the setup
+// 需要在设置之外使用
+export function usePermissionStoreWithOut() {
+  return usePermissionStore(store);
+}
