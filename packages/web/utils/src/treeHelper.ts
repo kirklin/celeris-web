3ae828b604ac0e9566180adeca354321faaecde3
Kirk Lin <linkirk@163.com>
2023-04-09 10:59:09 +0800

    feat(utils): add findTreeNode

--- packages/web/utils/src/treeHelper.ts
+++ packages/web/utils/src/treeHelper.ts
@@ -31,9 +31,9 @@ const getTreeHelperConfig = (config: Partial<TreeHelperConfig>): TreeHelperConfi
 /**
  * Convert a flat list to a tree structure.
  * 将列表转换为树形结构。
- * @param list Array<T> list
- * @param config Partial<TreeHelperConfig<T>> configuration options
- * @returns An array of type T[], where T extends TreeNode.
+ * @param list Array<T> list to be converted to a tree structure. 要转换为树形结构的列表。
+ * @param config Partial<TreeHelperConfig<T>> configuration options for TreeHelper. TreeHelper 的配置项。
+ * @returns An array of type T[], where T extends TreeNode. 返回树形结构，其中 T 继承自 TreeNode。
  */
 export function flattenToTree<T>(list: T[], config: Partial<TreeHelperConfig> = {}): T[] {
   const { idKey, childrenKey, parentKey } = getTreeHelperConfig(config);
@@ -62,13 +62,14 @@ export function flattenToTree<T>(list: T[], config: Partial<TreeHelperConfig> = 
 /**
  * Convert a tree structure to a flat list.
  * 将树形结构转换为扁平列表。
- * @param tree The tree structure to be converted to a flat list.
- * @param config The configuration for TreeHelper.
- * @returns An array of type T[] containing the flattened nodes, where T extends TreeNode.
+ * @param tree The tree structure to be converted to a flat list. 要转换为扁平列表的树形结构。
+ * @param config The configuration for TreeHelper. 树形结构的配置项。
+ * @returns An array of type T[] containing the flattened nodes, where T extends TreeNode. 返回扁平化的节点列表，其中 T 继承自 TreeNode。
  */
 export function flattenTree<T>(tree: T[], config: Partial<TreeHelperConfig> = {}): T[] {
   const { childrenKey } = getTreeHelperConfig(config);
   const flattenedNodes: T[] = [];
+
   function dfs(node: T) {
     flattenedNodes.push(node);
     const children = node[childrenKey];
@@ -78,8 +79,71 @@ export function flattenTree<T>(tree: T[], config: Partial<TreeHelperConfig> = {}
       }
     }
   }
+
   for (const node of tree) {
     dfs(node);
   }
   return flattenedNodes;
+}
+
+/**
+ * Find the first node in the tree that satisfies the predicate.
+ * 在树形结构中查找符合条件的第一个节点。
+ * @param treeData The root nodes of the tree. 树形结构的根节点列表。
+ * @param predicate The predicate function to test each node. 每个节点都会执行该回调函数。
+ * @param config The configuration for TreeHelper. 树形结构的配置项。
+ * @returns The first node in the tree that satisfies the predicate. 符合条件的第一个节点。
+ */
+export function findTreeNode<T>(
+  treeData: T[],
+  predicate: (node: T) => boolean,
+  config: Partial<TreeHelperConfig> = {},
+): T | null {
+  const { childrenKey } = getTreeHelperConfig(config);
+  for (const node of treeData) {
+    if (predicate(node)) {
+      return node;
+    }
+
+    const children = node[childrenKey];
+    if (children) {
+      const foundNode = findTreeNode(children, predicate, config);
+      if (foundNode) {
+        return foundNode;
+      }
+    }
+  }
+
+  return null;
+}
+
+/**
+ * Find all nodes in the tree that satisfy the predicate.
+ * 在树形结构中查找符合条件的所有节点。
+ * @param treeData The root nodes of the tree. 树形结构的根节点列表。
+ * @param predicate The predicate function to test each node. 每个节点都会执行该回调函数。
+ * @param config The configuration for TreeHelper. 树形结构的配置项。
+ * @returns An array of nodes that satisfy the predicate. 符合条件的节点组成的数组。
+ */
+export function findTreeNodes<T>(
+  treeData: T[],
+  predicate: (node: T) => boolean,
+  config: Partial<TreeHelperConfig> = {},
+): T[] {
+  const { childrenKey } = getTreeHelperConfig(config);
+  const result: T[] = [];
+
+  for (const node of treeData) {
+    if (predicate(node)) {
+      result.push(node);
+    }
+
+    const children = node[childrenKey];
+    if (children) {
+      const foundNodes = findTreeNodes(children, predicate, config);
+      result.push(...foundNodes);
+    }
+  }
+
+  return result;
 }
