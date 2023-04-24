a0e0ed789f6f5e9cd5457f400ce6c9a3f9c0bcbe
Kirk Lin <linkirk@163.com>
2023-04-24 09:56:45 +0800

    revert: optimize loading mock data in prod environment using `loadDataFromModules` function."
    This reverts commit 3ecc92e3f8ef6bfc07df3d9116329ea19d619602.

--- apps/admin/mock/_mock-server.ts
+++ apps/admin/mock/_mock-server.ts
@@ -1,11 +1,16 @@
-import { loadDataFromModules } from "@celeris/utils";
 import { createProdMockServer } from "vite-plugin-mock/es/createProdMockServer";
 
 const modules: Recordable = import.meta.glob("./**/*.ts", {
   import: "default",
   eager: true,
 });
-const mockModules = loadDataFromModules(modules);
+
+const mockModules = Object.keys(modules).reduce((pre, key) => {
+  if (!key.includes("/_")) {
+    pre.push(...modules[key]);
+  }
+  return pre;
+}, [] as any[]);
 
 /**
  * Used in a production environment. Need to manually import all modules
