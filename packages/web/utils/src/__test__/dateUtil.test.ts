48d1339f0f0de633b081485ab1684d3ba84f5801
Kirk Lin <linkirk@163.com>
2023-05-05 17:46:26 +0800

    test(utils): add dateUtil test

---
+++ packages/web/utils/src/__test__/dateUtil.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, test } from "vitest";
+import { formatToDate, formatToDateTime } from "../dateUtil";
+
+describe("formatToDate function", () => {
+  test("should return formatted date when given a valid date object", () => {
+    const validDate = new Date("2023-01-01T00:00:00.000Z");
+    const expectedOutput = "2023-01-01";
+    const actualOutput = formatToDate(validDate);
+    expect(actualOutput).toBe(expectedOutput);
+  });
+
+  test("should return formatted date with the given format when passed a valid date object and a format string", () => {
+    const validDate = new Date("2023-01-01T00:00:00.000Z");
+    const formatString = "YYYY-MM-DD";
+    const expectedOutput = "2023-01-01";
+    const actualOutput = formatToDate(validDate, formatString);
+    expect(actualOutput).toBe(expectedOutput);
+  });
+
+  test("should throw an error when passed an invalid date string", () => {
+    const invalidDate = "2018-10-10-10-10-10";
+    const expectedOutput = "Invalid Date";
+    const actualOutput = formatToDate(invalidDate);
+    expect(actualOutput).toBe(expectedOutput);
+  });
+});
+
+describe("formatToDateTime function", () => {
+  test("should return formatted date and time when given a valid date object", () => {
+    const validDate = new Date("2023-01-01T00:00:00.000Z");
+    const expectedOutput = "2023-01-01 08:00:00"; // assuming timezone offset is +08:00
+    const actualOutput = formatToDateTime(validDate);
+    expect(actualOutput).toBe(expectedOutput);
+  });
+
+  test("should throw an error when passed an invalid date string", () => {
+    const invalidDate = "2018-10-10-10-10-10";
+    const expectedOutput = "Invalid Date";
+    const actualOutput = formatToDateTime(invalidDate);
+    expect(actualOutput).toBe(expectedOutput);
+  });
+});
