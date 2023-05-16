c945b93928334d1f161d04e2c526f614e36e884c
Kirk Lin <linkirk@163.com>
2023-05-16 23:27:39 +0800

    feat(layouts): add fullscreen button

---
+++ apps/admin/src/layouts/header/components/FullScreenButton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import IconButtonWithToolTip from "~/layouts/header/components/IconButtonWithToolTip.vue";
+
+const { toggle: toggleFullScreen } = useFullscreen();
+</script>
+
+<template>
+  <IconButtonWithToolTip tooltip-text="Toggle FullScreen" icon="i-mdi-fullscreen" color="gray" size="5" @click="toggleFullScreen" />
+</template>
+
+<style scoped>
+
+</style>
--- apps/admin/src/layouts/header/index.vue
+++ apps/admin/src/layouts/header/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import CollapseButton from "~/layouts/header/components/CollapseButton.vue";
+import FullScreenButton from "~/layouts/header/components/FullScreenButton.vue";
 
 defineOptions({
   name: "Header",
@@ -7,10 +8,19 @@ defineOptions({
 </script>
 
 <template>
-  <div class="flex h-16 items-center ">
-    <div>
+  <div class="flex h-16 items-center">
+    <div class="hidden md:block px-4">
       <CollapseButton />
     </div>
+    <div class="flex-grow-1" />
+    <div class="flex">
+      <div class="flex items-center justify-center">
+        <div class="px-2">
+          <FullScreenButton />
+        </div>
+      </div>
+    </div>
+    <div class="hidden md:block px-4" />
   </div>
 </template>
 
