e8bd40d141a43946da034482cd0ec252b583fbf8
Kirk Lin <linkirk@163.com>
2023-05-28 12:19:00 +0800

    feat(scripts): add generate-tree scripts

---
+++ scripts/src/directoryTreeOrganizer.ts
@@ -0,0 +1,31 @@
+import * as path from "node:path";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import directoryTree from "directory-tree";
+import treeify from "treeify";
+
+const excludedDirectories = [/node_modules/, /\.git/, /\.vscode/, /\.idea/];
+const rootDirectory = process.cwd();
+const parentDirectory = path.dirname(rootDirectory);
+
+const getFilteredChildren = (root, excludes) => {
+  const filtered = directoryTree(root, { exclude: excludes });
+  return filtered.children || [];
+};
+
+const buildTree = (children) => {
+  return children.reduce((tree, child) => {
+    const hasChildren = child.children?.length > 0;
+    const formattedChild = hasChildren ? buildTree(child.children) : null;
+    return { ...tree, [child.name]: formattedChild };
+  }, {});
+};
+
+const formatTree = (object) => {
+  return treeify.asTree(object, true, null);
+};
+
+const formattedTree = formatTree(buildTree(getFilteredChildren(parentDirectory, excludedDirectories)));
+
+console.log(formattedTree);
