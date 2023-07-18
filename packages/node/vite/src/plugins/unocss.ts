c8d91cbf338b9395c59d3cbb4a0334f1db1a6c12
Kirk Lin <linkirk@163.com>
2023-07-18 17:25:20 +0800

    feat: add unocss shortcuts

--- packages/node/vite/src/plugins/unocss.ts
+++ packages/node/vite/src/plugins/unocss.ts
@@ -19,6 +19,33 @@ export function createUnoCSSPluginConfig(): PluginOption {
         warn: true,
       }),
     ],
+    shortcuts: {
+      // position
+      "pr": "relative",
+      "pa": "absolute",
+      "pf": "fixed",
+      "ps": "sticky",
+
+      // position layout
+      "position-x-center": "absolute left-1/2 -translate-x-1/2",
+      "pxc": "position-x-center",
+      "position-y-center": "absolute top-1/2 -translate-y-1/2",
+      "pyc": "position-y-center",
+      "position-center": "position-x-center position-y-center",
+      "pc": "position-center",
+
+      // size
+      "size-0": "w-0 h-0",
+      "size-full": "w-full h-full",
+      "size-screen": "w-screen h-screen",
+      "size-1/2": "w-1/2 h-1/2",
+
+      // flex layout
+      "flex-center": "flex justify-center items-center",
+      "flex-col-center": "flex-center flex-col",
+      "flex-x-center": "flex justify-center",
+      "flex-y-center": "flex items-center",
+    },
     transformers: [
       transformerDirectives(),
       transformerVariantGroup(),
