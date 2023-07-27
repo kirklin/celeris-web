9a403d39d5ac0bf410db493d4d29c6bc0d2c48ea
Kirk Lin <linkirk@163.com>
2023-07-27 15:15:16 +0800

    feat: add internationalization support for the login page

--- apps/admin/src/pages/login/components/LoginForm.vue
+++ apps/admin/src/pages/login/components/LoginForm.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
 import type { FormInst, FormRules } from "naive-ui";
+import { useI18n } from "@celeris/locale";
 import { useUserStore } from "~/store/modules/user";
 
 // 登录表单数据
 import type { LoginFromType } from "~/pages/login/types";
 
+const { t } = useI18n();
+
 const message = useMessage();
+const notification = useNotification();
 
 const loginFormModel: LoginFromType = reactive<LoginFromType>({
   username: "kirklin",
   password: "123456",
 });
 const loginRules: FormRules = reactive({
-  username: [{ required: true, message: "please input your username", trigger: "blur" }],
-  password: [{ required: true, message: "please input your password", trigger: "blur" }],
+  username: [{ required: true, message: t("page.login.form.username.error"), trigger: "blur" }],
+  password: [{ required: true, message: t("page.login.form.password.error"), trigger: "blur" }],
 });
 
 const loading = ref<boolean>(false);
@@ -32,15 +36,21 @@ const login = async () => {
 
     loading.value = true;
 
-    // Login the user
-    const userInfo = await useUserStore().login({
-      ...unref(loginFormModel),
-      remember: true,
-      errorMessageMode: "dialog",
-    });
-
-    if (userInfo) {
-      message.success("Login Success");
+    try {
+      // Login the user
+      const userInfo = await useUserStore().login({
+        ...unref(loginFormModel),
+        remember: true,
+        errorMessageMode: "none",
+      });
+      if (userInfo) {
+        notification.success({
+          title: t("page.login.notification.loginSuccessMessage"),
+          content: t("page.login.notification.welcomeBackMessage", { username: userInfo.fullName }),
+        });
+      }
+    } catch (error) {
+      message.error(t("page.login.form.incorrectAccountOrPassword"));
     }
   } finally {
     loading.value = false;
@@ -51,13 +61,13 @@ const login = async () => {
 <template>
   <NForm ref="loginFormRef" :model="loginFormModel" :rules="loginRules" size="large" class="mt-8 space-y-6">
     <div>
-      <NFormItem path="username" label="Your username">
-        <NInput v-model:value="loginFormModel.username" placeholder="input any username" />
+      <NFormItem path="username" :label="t('page.login.form.username.label')">
+        <NInput v-model:value="loginFormModel.username" :placeholder="t('page.login.form.username.placeholder')" />
       </NFormItem>
     </div>
     <div>
-      <NFormItem path="password" label="Your password">
-        <NInput v-model:value="loginFormModel.password" type="password" placeholder="••••••••" show-password-on="click" @keydown.enter="login" />
+      <NFormItem path="password" :label="t('page.login.form.password.label')">
+        <NInput v-model:value="loginFormModel.password" type="password" :placeholder="t('page.login.form.password.placeholder')" show-password-on="click" @keydown.enter="login" />
       </NFormItem>
     </div>
     <!--    <div class="flex items-start"> -->
@@ -70,7 +80,7 @@ const login = async () => {
     <!--      <a href="#" class="text-primary-700 dark:text-primary-500 ml-auto text-sm hover:underline">Lost Password?</a> -->
     <!--    </div> -->
     <NButton type="primary" :loading="loading" @click="login">
-      Login to your account
+      {{ t('page.login.form.loginButton') }}
     </NButton>
     <!--    <div class="text-sm font-medium text-gray-500 dark:text-gray-400"> -->
     <!--      Not registered? <a class="text-primary-700 dark:text-primary-500 hover:underline">Create account</a> -->
