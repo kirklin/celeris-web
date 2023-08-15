fcc757ae0f722f3046c88f2f9ef36b6173e19f6c
Kirk Lin <linkirk@163.com>
2023-08-15 18:13:27 +0800

    feat: custom common cubicBezier

--- apps/admin/src/store/modules/design/themeUtils.ts
+++ apps/admin/src/store/modules/design/themeUtils.ts
@@ -120,6 +120,9 @@ function getOtherTheme(darkMode: boolean): GlobalThemeOverrides {
   return {
     common: {
       borderRadius: "0.5rem",
+      cubicBezierEaseInOut: "cubic-bezier(0.65, 0, 0.35, 1)",
+      cubicBezierEaseOut: "cubic-bezier(0.33, 1, 0.68, 1)",
+      cubicBezierEaseIn: "cubic-bezier(0.32, 0, 0.67, 0)",
     },
     Card: {
       borderRadius: "1rem",
