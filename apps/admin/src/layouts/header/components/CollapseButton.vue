ffbfbb4c5a713a589d9c044505887f9aecb764df
Kirk Lin <linkirk@163.com>
2024-01-25 19:49:55 +0800

    feat: fix collapsed button icon

--- apps/admin/src/layouts/header/components/CollapseButton.vue
+++ apps/admin/src/layouts/header/components/CollapseButton.vue
@@ -3,8 +3,10 @@ import { useMenuSetting } from "~/composables/setting/useMenuSetting";
 import { ActionIcon } from "~/component/ActionIcon";
 
 const { t } = useI18n();
+
+const { toggleCollapsed, getCollapsed } = useMenuSetting();
 </script>
 
 <template>
-  <ActionIcon :tooltip-text="t('layouts.header.toggleCollapsed')" icon="i-tabler-menu-2" @click="useMenuSetting().toggleCollapsed()" />
+  <ActionIcon :tooltip-text="t('layouts.header.toggleCollapsed')" :icon="getCollapsed ? 'lucide:panel-right-close' : 'lucide:panel-left-close'" @click="toggleCollapsed()" />
 </template>
