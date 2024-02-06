b47bc233015ed4de62ccd1e9c3d64621b57405d6
Kirk Lin <linkirk@163.com>
2024-02-06 13:31:38 +0800

    fix: pagination infinite loop bug demo fix(#9)

--- apps/admin/src/pages/components/headless-table/pagination/index.vue
+++ apps/admin/src/pages/components/headless-table/pagination/index.vue
@@ -2,6 +2,7 @@
 import { FlexRender, createColumnHelper, getCoreRowModel, getPaginationRowModel, useVueTable } from "@tanstack/vue-table";
 
 import type { ChatGPTDemoTableRow } from "~/pages/components/headless-table/basic/data";
+import PageWrapper from "~/component/PageWrapper/src/PageWrapper.vue";
 
 defineOptions({
   name: "HeadlessTablePagination",
@@ -28,18 +29,19 @@ const columns = [
     footer: props => props.column.id,
   }),
 ];
+const chatGPTDemoTestData: ChatGPTDemoTableRow[] = [];
+onBeforeMount(() => {
+  for (let i = 0; i < 100; i++) {
+    chatGPTDemoTestData.push({
+      id: i,
+      userInput: `用户输入${i}`,
+      expectedReply: `预期回复${i}`,
+      isTestPassed: i % 2 === 0,
+    });
+  }
+});
 const table = useVueTable({
   get data() {
-    const chatGPTDemoTestData: ChatGPTDemoTableRow[] = [];
-    // 生成100次数据chatGPTDemoTestData
-    for (let i = 0; i < 100; i++) {
-      chatGPTDemoTestData.push({
-        id: i,
-        userInput: `用户输入${i}`,
-        expectedReply: `预期回复${i}`,
-        isTestPassed: i % 2 === 0,
-      });
-    }
     return chatGPTDemoTestData;
   },
   debugAll: false,
@@ -47,70 +49,79 @@ const table = useVueTable({
   getCoreRowModel: getCoreRowModel(),
   getPaginationRowModel: getPaginationRowModel(),
 });
+function handlePageChange(page: number) {
+  table.setPageIndex(page - 1);
+}
 function handlePageSizeChange(pageSize: number) {
   table.setPageSize(pageSize);
 }
 </script>
 
 <template>
-  <NCard title="分页表单">
-    <NTable :bordered="true" :single-line="false" striped>
-      <thead>
-        <tr
-          v-for="headerGroup in table.getHeaderGroups()"
-          :key="headerGroup.id"
-        >
-          <th
-            v-for="(header) in headerGroup.headers"
-            :key="header.id"
-            :colspan="header.colSpan"
-            class="font-bold!"
-            scope="col"
-          >
-            <FlexRender
-              :render="header.column.columnDef.header"
-              :props="header.getContext()"
-            />
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="row in table.getRowModel().rows"
-          :key="row.id"
-        >
-          <td
-            v-for="(cell) in row.getVisibleCells()"
-            :key="cell.id"
-          >
-            <div>
-              <div>
+  <PageWrapper>
+    <NScrollbar class="rounded-2xl">
+      <NCard title="分页表单">
+        <NTable :bordered="true" :single-line="false" striped>
+          <thead>
+            <tr
+              v-for="headerGroup in table.getHeaderGroups()"
+              :key="headerGroup.id"
+            >
+              <th
+                v-for="(header) in headerGroup.headers"
+                :key="header.id"
+                :colspan="header.colSpan"
+                class="font-bold!"
+                scope="col"
+              >
                 <FlexRender
-                  :render="cell.column.columnDef.cell"
-                  :props="cell.getContext()"
+                  :render="header.column.columnDef.header"
+                  :props="header.getContext()"
                 />
-              </div>
-            </div>
-          </td>
-        </tr>
-        <tr v-if="table.getRowModel().rows.length === 0">
-          <td
-            :colspan="table.getAllColumns().length"
-          >
-            <NEmpty />
-          </td>
-        </tr>
-      </tbody>
-    </NTable>
-    <NPagination
-      :default-page="Number(0)"
-      :page="table.getState().pagination.pageIndex"
-      :page-count="table.getPageCount()"
-      show-size-picker
-      :page-sizes="[10, 20, 30, 40, 50]"
-      @update:page-size="handlePageSizeChange"
-    />
-  </NCard>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="row in table.getRowModel().rows"
+              :key="row.id"
+            >
+              <td
+                v-for="(cell) in row.getVisibleCells()"
+                :key="cell.id"
+              >
+                <div>
+                  <div>
+                    <FlexRender
+                      :render="cell.column.columnDef.cell"
+                      :props="cell.getContext()"
+                    />
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr v-if="table.getRowModel().rows.length === 0">
+              <td
+                :colspan="table.getAllColumns().length"
+              >
+                <NEmpty />
+              </td>
+            </tr>
+          </tbody>
+        </NTable>
+        <template #action>
+          <NPagination
+            :page="table.getState().pagination.pageIndex + 1"
+            :page-count="table.getPageCount()"
+            show-size-picker
+            :page-sizes="[10, 20, 30, 40, 50]"
+            @update:page="handlePageChange"
+            @update:page-size="handlePageSizeChange"
+          />
+        </template>
+      </NCard>
+    </NScrollbar>
+  </PageWrapper>
 </template>
 
 <style scoped>
