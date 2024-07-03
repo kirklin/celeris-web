8a7bd197fe12be00b31d6255910f0f21663b2c84
Kirk Lin <linkirk@163.com>
2024-07-03 13:42:40 +0800

    chore: rename node packages

--- packages/web/components/Application/src/AppLogo.vue
+++ packages/web/components/Application/src/AppLogo.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, toRefs, withDefaults } from "vue";
+import { computed, toRefs } from "vue";
 import { PageConstants, RouterTransitionConstants } from "@celeris/constants";
 import { useRouter } from "vue-router";
 import { logoImage } from "@celeris/assets";
