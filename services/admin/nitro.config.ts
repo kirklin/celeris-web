93737351a272a99db269e12097516c91a6ad5324
Kirk Lin <linkirk@163.com>
2024-07-21 16:09:46 +0800

    feat: server cors

--- services/admin/nitro.config.ts
+++ services/admin/nitro.config.ts
@@ -2,6 +2,11 @@ import { defineNitroConfig } from "nitropack/config";
 
 export default defineNitroConfig({
   timing: true,
+  routeRules: {
+    "/**": {
+      cors: true,
+    },
+  },
   esbuild: {
     options: {
       target: "esnext",
