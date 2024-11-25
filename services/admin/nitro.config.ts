87bcbf7a7bc7e5009168bd3ee376052029fad8d0
Kirk Lin <linkirk@163.com>
2024-11-25 12:00:29 +0800

    chore: update deps

--- services/admin/nitro.config.ts
+++ services/admin/nitro.config.ts
@@ -2,14 +2,18 @@ import { defineNitroConfig } from "nitropack/config";
 
 export default defineNitroConfig({
   timing: true,
+
   routeRules: {
     "/**": {
       cors: true,
     },
   },
+
   esbuild: {
     options: {
       target: "esnext",
     },
   },
+
+  compatibilityDate: "2024-11-25",
 });
