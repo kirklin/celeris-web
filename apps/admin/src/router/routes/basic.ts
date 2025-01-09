5c7feea3f6bc81868043fa426557418234278c2f
Kirk Lin <linkirk@163.com>
2025-01-09 20:44:26 +0800

    fix(router): resolve page refresh 404 issue by adjusting route configuration

--- apps/admin/src/router/routes/basic.ts
+++ apps/admin/src/router/routes/basic.ts
@@ -9,6 +9,7 @@ import {
 // 404 on a page
 export const PAGE_NOT_FOUND_ROUTE: RouteRecordRaw = {
   path: "/PAGE_NOT_FOUND/:path(.*)*",
+  name: PAGE_NOT_FOUND_NAME,
   component: LAYOUT,
   meta: {
     title: "ErrorPage",
@@ -18,7 +19,6 @@ export const PAGE_NOT_FOUND_ROUTE: RouteRecordRaw = {
   children: [
     {
       path: "/:path(.*)*",
-      name: PAGE_NOT_FOUND_NAME,
       component: EXCEPTION_COMPONENT,
       meta: {
         title: "ErrorPage",
